763f93d0561737e3c0fc287b59ccfec404e61a50
HoangHien120398 <hoanghien120398@gmail.com>
2025-02-20 22:02:07 +0700

    change chrome to headless mode

--- src/test/java/supports/Browser.java
+++ src/test/java/supports/Browser.java
@@ -3,6 +3,7 @@ package supports;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -22,7 +23,9 @@ public class Browser {
         switch (browser) {
             case "chrome": {
 
-                driver = new ChromeDriver();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new");
+                driver = new ChromeDriver(chromeOptions);
                 break;
             }
             case "firefox": {
